647fae7ed58ff350f0d445f59a30c2505ba3e3dd
patinoman <68838876+patinoman@users.noreply.github.com>
2020-09-06 05:47:36 -0700

    readme and final

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,45 @@
 $(document).ready(function () {
   $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-  //Assign saveBtn click listener for user input and time stamp??
+  //saveBtn click listener
   $(".saveBtn").on("click", function () {
-    //get nearby values.
+    //This gets relative values
     console.log(this);
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    //set items in local storage.
+    //This sets items into localStorage
     localStorage.setItem(time, text);
   });
-  //load any saved data from LocalStorage - do this for each hour created.
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  //This loads data from localStorage
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
   function hourTracker() {
-    //get current number of hours.
-    var currentHour = moment().hour();
+    //Current number of hours.
+    var currentHour = moment().hours();
 
-    // loop over time blocks
+    // loop
     $(".time-block").each(function () {
       var blockHour = parseInt($(this).attr("id").split("hour")[1]);
       console.log(blockHour, currentHour);
 
-      //check if we've moved past this time
       if (blockHour < currentHour) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
+        $(this).addClass(".past");
       } else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
+        $(this).removeClass(".past");
+        $(this).addClass(".present");
       } else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-        console.log(hourTracker);
+        $(this).removeClass(".present");
+        $(this).removeClass(".past");
+        $(this).addClass(".future");
       }
     });
   }
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Work Day Scheduler Starter Code
+# **Work Day Scheduler**
+
+A simple calendar app for scheduling your workday!
+
+## Description
+
+---
+
+In this repository I created a simple calendar application that allows the user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery/Bootstrap ,along with Moment.js library to work with date and time. Tasks can be entered to a specific time, and the "save" button saves it to local storage.
+
+The application is generated using the following acceptance criteria:
+
+- GIVEN I am using a daily planner to create a schedule
+- WHEN I open the planner
+- THEN the current day is displayed at the top of the calendar
+- WHEN I scroll down
+- THEN I am presented with timeblocks for standard business hours
+- WHEN I view the timeblocks for that day
+- THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+- WHEN I click into a timeblock
+- THEN I can enter an event
+- WHEN I click the save button for that timeblock
+- THEN the text for that event is saved in local storage
+- WHEN I refresh the page
+- THEN the saved events persist
+
+## Process
+
+---
+
+I linked Bootstrap, jQuery, and Moment.js to my index.html starter code and created time blocks with the given CSS classes. I faced many challenges while finishing the code for this particular project, mostly due to configuring Moment.js and knowing the formatting for jQuery. There is still room for improvement and several areas of opportunity. I would have liked to add a clear function to have all tasks reset to its initial blank form. That is something I would like to challenge myself to refactor.
+
+## Credits
+
+---
+
+My friend Jesse and Jose, who have assisting me through my progress and many many MANY struggles.
+
+- https://stackoverflow.com/questions/9775375/parseint-with-jquery
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+- https://getbootstrap.com/docs/4.4/layout/overview/
+- https://momentjs.com/docs/#/displaying/
+
+## Screenshot
+
+---
+
+![Workday Planner](images\Capture.PNG)
